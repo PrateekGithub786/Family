7ee8c0e6b98ad7728a16ca47abe7483664e3b5d2
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-06-20 11:39:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/NT_EXMipV/model.json", modelLoaded);
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/7eUiUtY4V/model.json", modelLoaded);
  }
  
  function modelLoaded(){
